8b0052fbea06849f5944d6ec0896225d3c50ba64
inagdlog786 <34641425+inagdlog786@users.noreply.github.com>
2018-05-20 00:25:33 +0530

    Revert "POC"
    This reverts commit 7506a5ad2e126363060782b1a4ccb3198178f7af.

--- poc/app/_models/user.ts
+++
@@ -1,9 +0,0 @@
-export class User {
-    id: number;
-    username: string;
-    email: string;
-    password: string;
-    confirmPassword: string;
-    firstName: string;
-    lastName: string;
-}
